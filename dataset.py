4b02221af2bdadbdfe9d8f5736b726b8355bb8e8
佐藤更星 <dokkoi@dokkois-MacBook-Air.local>
2019-09-27 10:27:23 +0900

    share

---
+++ dataset.py
@@ -0,0 +1,111 @@
+#!/usr/bin/python
+# encoding: utf-8
+
+import random
+import torch
+from torch.utils.data import Dataset
+from torch.utils.data import sampler
+import torchvision.transforms as transforms
+from PIL import Image
+import numpy as np
+
+
+class txtDataset(Dataset):
+
+    def __init__(self, root=None, transform=None, target_transform=None):
+        with open(root, "r") as f:
+            anot_array = f.readlines()
+        self.transform = transform
+        self.target_transform = target_transform
+        self.data_num = len(anot_array)
+        self.data = []
+        self.label = []
+        for anot in anot_array:
+            anot = anot.split(" ")
+            self.data.append(anot[0])
+            self.label.append(anot[1])
+
+    def __len__(self):
+        return self.data_num
+
+    def __getitem__(self, idx):
+        out_img = Image.open("./IIIT5K/" + self.data[idx]).convert('L')
+        out_label = self.label[idx].rstrip("\n")
+
+        if self.transform:
+            out_img = self.transform(out_img)
+
+        if self.target_transform:
+            out_label = self.target_transform(out_label)
+
+        return out_img, out_label
+
+
+class resizeNormalize(object):
+
+    def __init__(self, size, interpolation=Image.BILINEAR):
+        self.size = size
+        self.interpolation = interpolation
+        self.toTensor = transforms.ToTensor()
+
+    def __call__(self, img):
+        img = img.resize(self.size, self.interpolation)
+        img = self.toTensor(img)
+        img.sub_(0.5).div_(0.5)
+        return img
+
+
+class randomSequentialSampler(sampler.Sampler):
+
+    def __init__(self, data_source, batch_size):
+        self.num_samples = len(data_source)
+        self.batch_size = batch_size
+
+    def __iter__(self):
+        n_batch = len(self) // self.batch_size
+        tail = len(self) % self.batch_size
+        index = torch.LongTensor(len(self)).fill_(0)
+        for i in range(n_batch):
+            random_start = random.randint(0, len(self) - self.batch_size)
+            batch_index = random_start + torch.range(0, self.batch_size - 1)
+            index[i * self.batch_size:(i + 1) * self.batch_size] = batch_index
+        # deal with tail
+        if tail:
+            random_start = random.randint(0, len(self) - self.batch_size)
+            tail_index = random_start + torch.range(0, tail - 1)
+            index[(i + 1) * self.batch_size:] = tail_index
+
+        return iter(index)
+
+    def __len__(self):
+        return self.num_samples
+
+
+class alignCollate(object):
+
+    def __init__(self, imgH=32, imgW=100, keep_ratio=False, min_ratio=1):
+        self.imgH = imgH
+        self.imgW = imgW
+        self.keep_ratio = keep_ratio
+        self.min_ratio = min_ratio
+
+    def __call__(self, batch):
+        images, labels = zip(*batch)
+
+        imgH = self.imgH
+        imgW = self.imgW
+        if self.keep_ratio:
+            ratios = []
+            for image in images:
+                w, h = image.size
+                ratios.append(w / float(h))
+            ratios.sort()
+            max_ratio = ratios[-1]
+            imgW = int(np.floor(max_ratio * imgH))
+            imgW = max(imgH * self.min_ratio, imgW)  # assure imgH >= imgW
+
+        transform = resizeNormalize((imgW, imgH))
+        images = [transform(image) for image in images]
+        images = torch.cat([t.unsqueeze(0) for t in images], 0)
+
+        return images, labels
